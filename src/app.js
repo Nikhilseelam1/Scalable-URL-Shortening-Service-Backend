3cb3bb6c399fc842527821951ac44ab4e174e633
Nikhil Seelam <nikhilseelam1@gmail.com>
2026-05-10 16:23:29 +0530

    project completed

--- src/app.js
+++ src/app.js
@@ -3,40 +3,42 @@ import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
 import router from "./routes/index.js";
 import * as urlController from "./controllers/url.controller.js";
-
 import errorMiddleware from "./middlewares/error.middleware.js";
 import notFoundMiddleware from "./middlewares/notFound.middleware.js";
 
 const app = express();
 
-app.use(helmet());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_ORIGIN,
+].filter(Boolean);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
+app.use(helmet());
 app.use(morgan("dev"));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 app.use("/api", router);
 
-
 app.get("/:shortCode", urlController.redirectUrl);
 
-
 app.use(notFoundMiddleware);
-
 app.use(errorMiddleware);
 
 export default app;
